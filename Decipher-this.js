66706a1f39bee428bd75e76a2378335d30e0bb6a
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-19 10:47:44 -0800

    Update Decipher-this.js

--- Decipher-this.js
+++ Decipher-this.js
@@ -15,3 +15,12 @@ Examples:
 
 
 // Solution
+
+function decipherThis(str) {
+  return str.split(" ")
+    .map(w => 
+      w.replace(/^\d+/, c => String.fromCharCode(c))
+      .replace(/^(.)(.)(.*)(.)$/, "$1$4$3$2")
+    )
+    .join(' ')
+}
